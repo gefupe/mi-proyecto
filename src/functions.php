372d02147e378a2b00b0f80858666931e1906a3e
gefupe <gefupe@alumnatflorida.es>
2024-09-18 20:52:14 +0200

    realizamos cambio en el archivo functions.php en la rama master

--- src/functions.php
+++ src/functions.php
@@ -5,8 +5,8 @@ function saludo(){
 
 saludo();
 
-function saludoPersonalizado($nombre){
-    echo "Hola " + $nombre;
+function saludoPersonalizado($nombre, $edad){
+    echo "Hola " + $nombre + ", ya tienes " + $edad + " años.";
 }
 
 saludoPersonalizado();
